412123168b7df6935ea30e3c852b00c3d274c871
ALEXDRUGS <105850199+ALEXDRUGS@users.noreply.github.com>
2022-12-05 12:24:01 +0300

    ListRefactor + task2 переименовал

--- src/example/ListRefactor.java
+++ src/example/ListRefactor.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class ListRefactor {
     public static Map<String, List<Integer>> stringListHashMap = new HashMap<>();
 
-    public List<Integer> add() {
+    public List<Integer> listGenerator() {
         List<Integer> integerList = new ArrayList<>(10);
         for (int i = 0; i < 10; i++) {
             Random random = new Random();
@@ -27,7 +27,7 @@ public class ListRefactor {
 
     public static void main(String[] args) {
         ListRefactor listRefactor = new ListRefactor();
-        List<Integer> test = listRefactor.add();
+        List<Integer> test = listRefactor.listGenerator();
         stringListHashMap.put("nums", test);
         System.out.println(stringListHashMap);
         stringListHashMap.replace(stringListHashMap.entrySet().iterator().next().getKey(), getSum(test));
